6424018d63cc81ee8d6b8a8e6682e3eea5b07098
devCurti <joaocurti2002@gmail.com>
2024-05-22 20:44:59 -0300

    Iniciado back-end
    Iniciado back-end

---
+++ BackEnd/AEPBackend/src/main/java/com/AEPBackend/AEPBackend/AepBackendApplication.java
@@ -0,0 +1,13 @@
+package com.AEPBackend.AEPBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AepBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AepBackendApplication.class, args);
+	}
+
+}
---
+++ BackEnd/AEPBackend/src/test/java/com/AEPBackend/AEPBackend/AepBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AEPBackend.AEPBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AepBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
